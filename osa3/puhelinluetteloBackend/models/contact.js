0567a3e95bfe78ee64b71b89b5a5e24e8393073d
Toivo Närhi <toivo.narhi@helsinki.fi>
2025-05-25 16:45:59 +0300

    uibuild

--- osa3/puhelinluetteloBackend/models/contact.js
+++ osa3/puhelinluetteloBackend/models/contact.js
@@ -13,6 +13,7 @@ const contactSchema = new mongoose.Schema({
     },
     number:{
         type: String,
+        minlength: 8,
         validate: {
         validator: function(v) {
         return /^\d{2,3}-\d{5,10}$/.test(v);
