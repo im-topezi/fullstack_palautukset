a3dc0151220bd234de7d802e3686edd18cb15631
im-topezi <toivo.narhi@helsinki.fi>
2025-11-26 15:21:48 +0200

    Update vite.config.js

--- osa2/maa-palvelu/vite.config.js
+++ osa2/maa-palvelu/vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+        allowedHosts: true,
+    },
+  });
+};
 
 // https://vite.dev/config/
 export default defineConfig({
